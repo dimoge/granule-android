80d4d66c1e745a85b1bddc74bf521d9cb698dab4
dimoge <guohao_pq@163.com>
2015-08-07 16:25:16 +0800

    项目导出部分修改

---
+++ src/main/java/granule/dimoge/me/AppStart.java
@@ -0,0 +1,14 @@
+package granule.dimoge.me;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AppStart extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+
+}
