6f340a1023393a7392a239434ab52cc5f6140426
dimoge <guohao_pq@163.com>
2015-08-10 12:41:11 +0800

    修改创建账户对话框布局文件, 添加id,添加帮助类

--- src/main/java/granule/dimoge/me/dialog/AccountCreateDialog.java
+++ src/main/java/granule/dimoge/me/dialog/AccountCreateDialog.java
@@ -1,9 +1,13 @@
 package granule.dimoge.me.dialog;
 
+import android.app.ActionBar;
 import android.app.Dialog;
 import android.content.Context;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import granule.dimoge.me.R;
+import granule.dimoge.me.utils.CommonUtil;
 
 /**
  * Created by dime on 2015/8/8 0008.
@@ -11,9 +15,12 @@ import granule.dimoge.me.R;
 public class AccountCreateDialog extends Dialog {
 
     Window window;
+    Context context;
 
     public AccountCreateDialog(Context context, int theme) {
         super(context, theme);
+        this.context = context;
+
     }
 
     /**
@@ -22,6 +29,10 @@ public class AccountCreateDialog extends Dialog {
     public void showDialog(){
         setContentView(R.layout.dialog_account_create);
         window = getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.width = CommonUtil.dip2px(context,350);
+        layoutParams.height = CommonUtil.dip2px(context, 300);
+        window.setAttributes(layoutParams);
         setCanceledOnTouchOutside(false);//不允许触摸消失
         show();//显示出来
     }
---
+++ src/main/java/granule/dimoge/me/utils/CommonUtil.java
@@ -0,0 +1,22 @@
+package granule.dimoge.me.utils;
+
+import android.content.Context;
+
+/**
+ * Created by dime on 2015/8/10 0010.
+ */
+public class CommonUtil {
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
