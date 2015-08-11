3154fef862f6a1b99538637cc53610725a35e6a9
dimoge <guohao_pq@163.com>
2015-08-11 17:48:12 +0800

    账户界面使用假数据加载适配器成功, addFooter出错

--- src/main/java/granule/dimoge/me/activity/AccountActivity.java
+++ src/main/java/granule/dimoge/me/activity/AccountActivity.java
@@ -1,12 +1,22 @@
 package granule.dimoge.me.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import granule.dimoge.me.AppConfig;
 import granule.dimoge.me.R;
+import granule.dimoge.me.adapter.AccountListAdapter;
 import granule.dimoge.me.dialog.AccountCreateDialog;
+import granule.dimoge.me.entity.Account;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by dime on 2015/8/8 0008.
@@ -14,7 +24,8 @@ import granule.dimoge.me.dialog.AccountCreateDialog;
 public class AccountActivity extends Activity implements View.OnClickListener {
     Context context;
 
-    ImageButton account_add_btn;//添加账户的按钮
+    ImageView account_add_img;//添加账户的按钮
+    ListView account_lv;//账户列表
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +40,7 @@ public class AccountActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.account_add_btn:
+            case 1:
                 //TODO:添加新账户
                 new AccountCreateDialog(context, R.style.account_create_style).showDialog();
                 break;
@@ -41,14 +52,41 @@ public class AccountActivity extends Activity implements View.OnClickListener {
      * 控件初始化
      */
     private void initView() {
-        account_add_btn = (ImageButton) findViewById(R.id.account_add_btn);
+        account_lv = (ListView) findViewById(R.id.account_lv);
+        /////TODO:模拟假数据显示listview
+        List<Account> accountList = new ArrayList<Account>();
+        for(int i=0 ; i<5 ; i++ ){
+            Account account = new Account();
+            account.setId(i);
+            account.setUserId(AppConfig.user.getId());
+            account.setImg(R.mipmap.cash + "");
+            account.setTotal(1024);
+            account.setExpend(77);
+            account.setIncome(66);
+            account.setDate(new Date());
+            accountList.add(account);
+        }//数据准备完毕
+        account_add_img = new ImageView(context);
+        account_add_img.setImageResource(R.mipmap.account_add);
+        AbsListView.LayoutParams params = new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        account_add_img.setLayoutParams(params);
+        account_add_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //添加用户
+                new AccountCreateDialog(context, R.style.account_create_style).showDialog();
+            }
+        });
+        account_lv.addFooterView(account_add_img);
+        AccountListAdapter accountListAdapter = new AccountListAdapter(context, accountList);
+        account_lv.setAdapter(accountListAdapter);
+
     }
 
     /**
      * 点击事件初始化
      */
     private void initClick() {
-        account_add_btn.setOnClickListener(this);
     }
 
 
---
+++ src/main/java/granule/dimoge/me/adapter/AccountListAdapter.java
@@ -0,0 +1,62 @@
+package granule.dimoge.me.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import granule.dimoge.me.R;
+import granule.dimoge.me.entity.Account;
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by dime on 2015/8/11 0011.
+ */
+public class AccountListAdapter extends BaseAdapter {
+
+
+    Context context;
+    List<Account> accountList;
+
+    ImageView account_icon_img;//账户图标
+    TextView account_name_tv, account_total_tv;//账户名称, 账户总额
+    ImageButton account_update_imgBtn;//账户信息修改
+
+    public AccountListAdapter(Context context, List<Account> accountList) {
+        this.accountList = accountList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return accountList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return accountList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.adapter_account, null);//加载布局文件
+            //控件初始化
+            account_icon_img = (ImageView) convertView.findViewById(R.id.account_icon_img);
+            account_name_tv = (TextView) convertView.findViewById(R.id.account_name_tv);
+            account_total_tv = (TextView) convertView.findViewById(R.id.account_total_tv);
+            account_update_imgBtn = (ImageButton) convertView.findViewById(R.id.account_update_imgBtn);
+        }
+        return convertView;
+    }
+}
